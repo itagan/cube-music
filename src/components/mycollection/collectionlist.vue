6a2070f827bd5969314c3efbb160acf29d8ad4b4
agan <agan@agandeMacBook-Pro.local>
2019-12-12 22:59:57 +0800

    头部功能

---
+++ src/components/mycollection/collectionlist.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <my-header></my-header>
+    <div>000</div>
+  </div>
+</template>
+
+<script>
+    import myHeader from './header'
+    export default {
+        name: "myCollectionList.vue",
+        components: {
+            myHeader
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/mycollection/search.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <my-search
+      slot="center"
+      :placeholder="placeholderOne"
+      fake
+      @query="getQuery"
+      @click.native="goToSearch"
+    ></my-search>
+
+    <cube-input v-model="value" ></cube-input>
+  </div>
+</template>
+
+<script>
+    import MySearch from '../../base/search/search'
+    export default {
+        name: "search.vue",
+        components: {
+            MySearch
+        },
+        data () {
+          return {
+              value: '',
+              placeholderOne:'真的爱你',
+              placeholder: '请输入内容',
+              readonly: true,
+              maxlength: 100,
+              disabled: true,
+              autofocus: true,
+              autocomplete: true,
+          }
+        },
+        methods: {
+          getQuery (query) {
+              console.log(query)
+          },
+          goToSearch () {
+              this.$router.push('/my')
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
